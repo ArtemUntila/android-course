bafd9a05d39f7194b960901cdfc8b2426d9de9ef
Artyom <untila_artem@mail.ru>
2021-10-22 16:00:04 +0300

    lab03 - NavigationDrawer

---
+++ lab03/src/main/java/me/artyom/androidcourse/lab03/MainActivity.kt
@@ -0,0 +1,46 @@
+package me.artyom.androidcourse.lab03
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import me.artyom.androidcourse.lab03.task5.R
+import me.artyom.androidcourse.lab03.task5.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBar.toolbar)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home), binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    startActivity(Intent(this, ActivityAbout::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean =
+        navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+}
